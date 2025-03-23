8f5596eae6dbd9bfb631b80b13c09298b9759093
Nessa <nessaprogramming@gmail.com>
2025-03-23 22:10:58 +0700

    Learn Showing alert messages

--- day-20/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ day-20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,54 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingAlert = false
+    
     var body: some View {
         
-        // cara 1
-//        Button("Delete selection") {
-//            print("Now deleting...")
-//        }
-        
-        // cara 2
-//        Button("Deleting selection", role: .destructive, action: executeDelete)
-        
-//        VStack {
-//            Button("Button 1"){}
-//                .buttonStyle(.bordered)
-//            
-//            Button("Button 2", role: .destructive){}
-//                .buttonStyle(.bordered)
-//            
-//            Button("Button 3"){}
-//                .buttonStyle(.borderedProminent)
-//                .tint(.indigo)
-//            
-//            Button("Button 4", role: .destructive){}
-//                .buttonStyle(.borderedProminent)
-//        }
-        
-        // custom button style
-        Button {
-            print("Button was tapped")
-        } label: {
-            //            Text("Tap me!")
-            //                .padding()
-            //                .foregroundStyle(.white)
-            //                .background(.red)
-            
-            Label("Edit", systemImage: "pencil")
-                .padding()
-                .foregroundStyle(.white)
-                .background(.red)
+        Button("Show alert") {
+            showingAlert = true
         }
-        
-//        Image(systemName: "pencil.circle")
-//            .foregroundStyle(.red)
-//            .font(.largeTitle)
-        
-    }
-    
-    func executeDelete() {
-        print("Now deleting...")
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) { }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Please read this important alert!")
+        }
     }
 }
 
