ac5de891357c5c81cff46a81ea27ef921ca08bff
Nessa <nessaprogramming@gmail.com>
2025-03-30 01:03:57 +0700

    learn view composition

--- day-23/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ day-23/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,24 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ViewBuilder var spells: some View {
-        Text("Lumos")
-        Text("Obliviate")
-    }
-    var motto1: some View {
-        Text("Draco dormiens")
-    }
-    let motto2 = Text("nunquam titillandus")
     
     var body: some View {
-        VStack {
-            motto1
-                .foregroundStyle(.red)
-            motto2
-                .foregroundStyle(.blue)
-            
-            spells
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+                .foregroundStyle(.white)
+            CapsuleText(text: "Second")
+                .foregroundStyle(.yellow)
         }
+    }
+}
+
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .background(.blue)
+            .clipShape(.capsule)
     }
 }
 
