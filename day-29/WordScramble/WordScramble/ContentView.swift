e92c54b06d62be6e6b8be131c80744733935d27e
Nessa <nessaprogramming@gmail.com>
2025-04-03 18:41:52 +0700

    learn Loading resources from your app bundle

--- day-29/WordScramble/WordScramble/ContentView.swift
+++ day-29/WordScramble/WordScramble/ContentView.swift
@@ -8,47 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
-    
     var body: some View {
-        
-        List {
-            Text("Static Row")
-            
-            ForEach(people, id: \.self) {
-                Text($0)
+        Text("Hellow nessa!")
+    }
+    
+    func testBundles() {
+        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+            if let fileContents = try? String(contentsOf: fileURL) {
+                // we loaded the file into a string!
             }
-            
-            Text("Static Row")
         }
-        
-//        List(people, id: \.self) {
-//            Text($0)
-//        }
-        
-//        List {
-//            Section ("Section 1") {
-//                Text("Static row 1")
-//                Text("Static row 2")
-//            }
-//            
-//            Section("Section 2") {
-//                ForEach(0..<5) {
-//                    Text("Dynamic row \($0)")
-//                }
-//            }
-//            
-//            Section("Section 3") {
-//                Text("Static row 3")
-//                Text("Static row 4")
-//            }
-//        }
-//        .listStyle(.grouped)
-        
-        // if your entire list is made up of dynamic rows
-//        List(0..<5) {
-//            Text("Dynamic row \($0)")
-//        }
     }
 }
 
