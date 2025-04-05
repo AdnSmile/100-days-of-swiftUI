a7e78e094a4a47689ba8fef1d142641c3cfc645f
Nessa <nessaprogramming@gmail.com>
2025-04-05 21:17:42 +0700

    learn Controlling the animation stack

---
+++ day-33/Animations/Animations/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Nessa on 05/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var enabled = false
+    
+    var body: some View {
+        Button("Tap Me") {
+            enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .red)
+        .foregroundStyle(.white)
+        .animation(.default, value: enabled)
+        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
+    }
+}
+
+#Preview {
+    ContentView()
+}
