2b8bab755260a8478a3e8e1da42948bf95ceb458
Nessa <nessaprogramming@gmail.com>
2025-04-05 20:24:10 +0700

    learn Animating bindings

--- day-32/Animations/Animations/ContentView.swift
+++ day-32/Animations/Animations/ContentView.swift
@@ -11,38 +11,25 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap me") {
-//            animationAmount += 0.5
-        }
-            .padding(50)
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale Amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
             .background(.red)
             .foregroundStyle(.white)
             .clipShape(.circle)
-//            .scaleEffect(animationAmount)
-//            .blur(radius: (animationAmount - 1) * 3)
-//            .animation(.default, value: animationAmount)
-//            .animation(.linear, value: animationAmount)
-//            .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
-                        .easeInOut(duration: 1)                .repeatForever(autoreverses: false),
-                        value: animationAmount
-                    )
-            )
-            .onAppear{
-                animationAmount = 2
-            }
-//            .animation(
-//                .easeInOut(duration: 1)
-////                    .delay(1),
-////                    .repeatCount(3, autoreverses: true),
-//                    .repeatForever(autoreverses: true),
-//                value: animationAmount
-//            )
+            .scaleEffect(animationAmount)
+        }
     }
 }
 
