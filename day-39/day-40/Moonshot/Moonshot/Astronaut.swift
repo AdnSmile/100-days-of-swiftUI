7acd244467e620dc7fcab3654a5c9d713c7ecc22
Nessa <nessaprogramming@gmail.com>
2025-07-05 22:57:25 +0700

    Complete day 40

---
+++ day-39/day-40/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Nessa on 05/07/25.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ day-39/day-40/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Nessa on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
